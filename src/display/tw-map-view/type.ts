737ca5f4b2724d78049c6150e00f00cb92cc69b3
muhaoxing <571293251@qq.com>
2022-12-05 10:54:49 +0800

    remove copyright

--- src/display/tw-map-view/type.ts
+++ src/display/tw-map-view/type.ts
@@ -1,15 +1,3 @@
-/*
- * Copyright (c) 2019 3will Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the 3will License v1.0
- * which accompanies this distribution, and is available at
- *
- *     http://www.3will.cn/licenses/license-v1.0
- *
- * Contributors:
- *    3will - initial API and implementation
- */
-
 import type { NativeSyntheticEvent } from 'react-native';
 import * as React from 'react';
 import { ImageSourcePropType, ViewProps } from 'react-native';
