4ae57dee4964a12ef99354e699cc82dd8306e793
muhaoxing <571293251@qq.com>
2022-12-05 10:52:50 +0800

    update

--- src/display/tw-map-view/TwMapView.native.tsx
+++ src/display/tw-map-view/TwMapView.native.tsx
@@ -13,8 +13,8 @@ import React, { FC, ForwardRefExoticComponent } from 'react';
 import { MarkerProps, TwMapViewProps } from './type';
 import { AMapSdk, MapView, Marker } from 'react-native-amap3d';
 
+// @ts-ignore
+export const TwMapView: ForwardRefExoticComponent<TwMapViewProps> = MapView as any;
 //@ts-ignore
-// export const TwMapView: ForwardRefExoticComponent<TwMapViewProps> = MapView as any;
-// //@ts-ignore
-// export const TwMapMaker: FC<MarkerProps> = Marker as any;
-// export const TwMapViewSdk = AMapSdk;
+export const TwMapMaker: FC<MarkerProps> = Marker as any;
+export const TwMapViewSdk = AMapSdk;
